7f03bcfd0d344827a28941f8f4af748010919942
nikitatokarev <tokarev_ns@proitr.ru>
2023-04-23 04:20:06 +0200

    Category creation has been added

--- niffler-e-2-e-tests/src/test/java/niffler/test/SpendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/test/SpendsWebTest.java
@@ -1,53 +1,61 @@
 package niffler.test;
 
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.AllureId;
+import niffler.jupiter.annotation.GenerateCategory;
+import niffler.jupiter.annotation.GenerateSpend;
+import niffler.jupiter.extension.GenerateCategoryExtension;
+import niffler.jupiter.extension.GenerateSpendExtension;
+import niffler.model.CurrencyValues;
+import niffler.model.SpendJson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Selenide;
-import io.qameta.allure.AllureId;
-import niffler.jupiter.annotation.GenerateSpend;
-import niffler.model.CurrencyValues;
-import niffler.model.SpendJson;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
-@Disabled
+@ExtendWith(GenerateCategoryExtension.class)
+@ExtendWith(GenerateSpendExtension.class)
 public class SpendsWebTest extends BaseWebTest {
 
     @BeforeEach
     void doLogin() {
         Selenide.open("http://127.0.0.1:3000/main");
         $("a[href*='redirect']").click();
-        $("input[name='username']").setValue("dima");
+        $("input[name='username']").setValue("Shprotestant");
         $("input[name='password']").setValue("12345");
         $("button[type='submit']").click();
     }
 
+    @GenerateCategory(
+            category = "SomeCategory",
+            username = "Shprotestant"
+    )
+
     @GenerateSpend(
-        username = "dima",
-        description = "QA GURU ADVANCED VOL 2",
-        currency = CurrencyValues.RUB,
-        amount = 52000.00,
-        category = "Обучение"
+            username = "Shprotestant",
+            description = "QA GURU ADVANCED VOL 2",
+            currency = CurrencyValues.RUB,
+            amount = 52000.00,
+            category = "SomeCategory"
     )
     @AllureId("101")
     @Test
     void spendShouldBeDeletedByActionInTable(SpendJson spend) {
         $(".spendings-table tbody").$$("tr")
-            .find(text(spend.getDescription()))
-            .$$("td").first()
-            .scrollTo()
-            .click();
+                .find(text(spend.getDescription()))
+                .$$("td").first()
+                .scrollTo()
+                .click();
 
         $$(".button_type_small").find(text("Delete selected"))
-            .click();
+                .click();
 
         $(".spendings-table tbody")
-            .$$("tr")
-            .shouldHave(CollectionCondition.size(0));
-        throw new IllegalStateException();
+                .$$("tr")
+                .shouldHave(CollectionCondition.size(0));
     }
 }
